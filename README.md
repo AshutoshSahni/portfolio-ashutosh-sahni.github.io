7113aa98affe84893a92de0defa9aae295378c41
Ashutosh Sahni <iashutoshsahni@gmail.com>
2023-03-06 22:28:32 +0530

    adding project files

---
+++ README.md
@@ -0,0 +1,1 @@
+#portfolio - Ashutosh Sahni
---
+++ script.js
@@ -0,0 +1,11 @@
+function showProfile() {
+    document.getElementById("profile").style.display = "flex";
+      document.getElementById("skills").style.display = "none";
+  }
+  
+  function showSkills() {
+    document.getElementById("profile").style.display = "none";
+      document.getElementById("skills").style.display = "flex";
+  }
+  
+  
