80ca9adf14817a907c67700b0214426c650a5a61
Ashutosh Sahni <iashutoshsahni@gmail.com>
2023-03-06 23:26:28 +0530

    show active section in nav bar

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+var btns = document.querySelectorAll('.nav-btn');
+
+function makeNormal() {
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].style.fontWeight = "normal";
+    btns[i].style.color = "#adb5bd";
+  }
+}
+
 function showProfile() {
   document.getElementById("profile").style.display = "flex";
   document.getElementById("skills").style.display = "none";
+
+  makeNormal();
   document.getElementById("b_about").style.fontWeight = "bold";
-  document.getElementById("b_about").style.color = "#dee2e6";
+  document.getElementById("b_about").style.color = "#f8f9fa";
+
 
 }
 
 function showSkills() {
   document.getElementById("profile").style.display = "none";
   document.getElementById("skills").style.display = "flex";
-  document.getElementById("b_skills").style.fontWeight = "bold";
-  document.getElementById("b_skills").style.color = "#dee2e6";
+
+  makeNormal();
+  document.getElementById("b_skills").style.fontWeight = "bold"
+  document.getElementById("b_skills").style.color = "#f8f9fa";
+
 }
 
