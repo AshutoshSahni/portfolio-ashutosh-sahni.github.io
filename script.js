540e597d15735ebb73739701536f80f52f8661d2
Ashutosh Sahni <iashutoshsahni@gmail.com>
2023-03-06 22:47:39 +0530

    added ids for nav buttons

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function showProfile() {
-    document.getElementById("profile").style.display = "flex";
-      document.getElementById("skills").style.display = "none";
-  }
-  
-  function showSkills() {
-    document.getElementById("profile").style.display = "none";
-      document.getElementById("skills").style.display = "flex";
-  }
-  
-  
+  document.getElementById("profile").style.display = "flex";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("b_about").style.fontWeight = "bold";
+  document.getElementById("b_about").style.color = "#dee2e6";
+
+}
+
+function showSkills() {
+  document.getElementById("profile").style.display = "none";
+  document.getElementById("skills").style.display = "flex";
+  document.getElementById("b_skills").style.fontWeight = "bold";
+  document.getElementById("b_skills").style.color = "#dee2e6";
+}
+
